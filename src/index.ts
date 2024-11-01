22ff6c6ad19e768b76a6dc0fd194e4b8509a2e93
aksu <scyzhe@hotmail.com>
2024-11-01 12:30:54 +0200

    prototyping mmlapi 2m tif

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,77 @@ app.post('/api/2m', async (request, response) => {
     // get tif(elevationmap) download url path from maanmittauslaitos api
     const result = await mmlElevation2mBBOX(bbox)
 
+    if (result === undefined){
+        response.status(503).json({message: 'maanmittauslaitos issue.'})
+        return
+    }
+
     response.json({path: result})
 })
+
+
+import { writeFile } from "fs";
+import axios from "axios";
+const downloadTif = async (urlPath: string) => {
+    return axios
+            .get(urlPath, {responseType: 'arraybuffer'})
+            .then(response => {
+                /*
+                writeFile('./src/files/test/dl_test.tif', response.data, {encoding: null}, (err) =>{
+                    if (err) throw err
+
+                })
+                    */
+                return response.data
+            })
+            .catch(e => console.log('error:', e))
+}
+app.post('/api/2mtif', async (request, response) => {
+    // add validation
+    // The numbers are in decimal degrees format and range from -90 to 90 for latitude and -180 to 180 for longitude.
+
+    interface requestBody {
+        latitude: number, 
+        longitude: number
+    }
+
+    // lat long to coordinates const
+    const coordiantes: { latitude: number, longitude: number } = request.body
+
+    // convert wgs84 lat long to etrs89 east north
+    const [easting, northing] = WGS84toETRS89(coordiantes.latitude, coordiantes.longitude)
+
+    //create 1 square km bounding box from position.
+    const bbox = bboxFromETRS89(easting, northing)
+
+    // get tif(elevationmap) download url path from maanmittauslaitos api
+    const result = await mmlElevation2mBBOX(bbox)
+
+    if (result === undefined){
+        response.status(503).json({message: 'maanmittauslaitos issue.'})
+        return
+    }
+
+    const tifFile = await downloadTif(result)
+
+    response.setHeader('Content-Type', 'image/tiff')
+    response.setHeader('Content-Disposition', 'attachment; filename=img.tif')
+    response.send(Buffer.from(tifFile, 'utf-16le'))
+})
+/* works..
+const downloadTif = async (urlPath: string) => {
+    return axios
+            .get(urlPath, {responseType: 'arraybuffer'})
+            .then(response => {
+                writeFile('./src/files/test/dl_test.tif', response.data, {encoding: null}, (err) =>{
+                    if (err) throw err
+
+                })
+                return response.data
+            })
+            .catch(e => console.log('error:', e))
+}
+*/
 
 import { WGS84toETRS89, bboxFromETRS89 } from "./geoStuff/WGS84toETRS89";
 app.post('/api', (request, response) => {
